0d818bc7371bc1a2600dba50144841c804d1f020
Nick Frost <nickfrost.us@gmail.com>
2021-03-10 12:07:10 -0500

    date and time in taskbar

--- src/components/Taskbar/Taskbar.vue
+++ src/components/Taskbar/Taskbar.vue
@@ -3,12 +3,44 @@
     <button class="start-button">
       <windows-logo/>
     </button>
+
+    <div class="taskbar__datetime">
+      <div>{{ time }}</div>
+      <div>{{ date }}</div>
+    </div>
   </div>
 </template>
 
+<script>
+import WindowsLogo from "@/components/WindowsLogo";
+import date from 'date-and-time'
+
+export default {
+  components: {WindowsLogo},
+  data() {
+    return {
+      updateInterval: null,
+      date: null,
+      time: null
+    }
+  },
+  mounted() {
+    this.updateInterval = setInterval(() => {
+      const now = new Date();
+      this.date = date.format(now, 'M/D/YYYY');
+      this.time = date.format(now, 'hh:mm A');
+    }, 1000);
+  }
+}
+</script>
+
 <style lang="scss">
 .taskbar {
-  @apply fixed bottom-0 right-0 left-0 h-12 bg-gray-900 opacity-95;
+  @apply fixed flex justify-start items-center bottom-0 right-0 left-0 h-12 bg-gray-900 opacity-95;
+
+  &__datetime {
+    @apply flex flex-col justify-center items-center ml-auto text-white pr-3 text-xs;
+  }
 }
 
 .start-button {
@@ -19,9 +51,3 @@
   }
 }
 </style>
-<script>
-import WindowsLogo from "@/components/WindowsLogo";
-export default {
-  components: {WindowsLogo}
-}
-</script>
